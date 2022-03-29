f7545a2fef2666fa900ec2272da8f1130409cfb1
Gitesh Patil <patilgitesh29@gmail.com>
2022-03-30 01:31:29 +0530

    Minor changes

--- medical-records-on-blockchain/src/Components/Features.js
+++ medical-records-on-blockchain/src/Components/Features.js
@@ -10,21 +10,21 @@ export default function Features() {
                             {/* <svg className="bi" width="1em" height="1em"><use xlink:href="#collection"></use></svg> */}
                         </div>
                         <h5 className='feature-title'>Our Story</h5>
-                        <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
+                        <p className='feature-text'>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
                     </div>
                     <div className="feature col-4 m-auto feature-item">
                         <div className="feature-icon bg-primary bg-gradient">
                             {/* <svg className="bi" width="1em" height="1em"><use xlink:href="#people-circle"></use></svg> */}
                         </div>
                         <h5 className='feature-title'>Our Vision</h5>
-                        <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
+                        <p className='feature-text'>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
                     </div>
                     <div className="feature col-4 m-auto feature-item">
                         <div className="feature-icon bg-primary bg-gradient">
                             {/* <svg className="bi" width="1em" height="1em"><use xlink:href="#toggles2"></use></svg> */}
                         </div>
                         <h5 className='feature-title'>Technology</h5>
-                        <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
+                        <p className='feature-text'>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
                     </div>
                 </div>
             </div>
