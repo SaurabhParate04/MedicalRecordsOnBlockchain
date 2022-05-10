48ecf0084e52d5f651200d0d9867b1c0267b39ba
Gitesh Patil <patilgitesh29@gmail.com>
2022-05-10 20:49:13 +0530

    Some style chnages

--- medical-records-on-blockchain/src/Components/Form.js
+++ medical-records-on-blockchain/src/Components/Form.js
@@ -7,17 +7,17 @@ export default function Form() {
   const duplicate = () => {
     var clone = original.cloneNode(true);
     clone.id = ""; 
-    original.parentNode.appendChild(clone);
+    original.parentNode.appendChild(clone); 
   } 
  
   return ( 
     <div>
-      <div className='container pt-4'>    
-        <h2 className='mb-3 text-center fw-bold'>Form</h2> 
+      <div className='container border mt-5 p-4 ' style={{backgroundColor:"#061551", boxShadow:"0 10px 10px rgba(0,0,0,0.22)"}}>    
+        <h2 className='mb-3 text-center text-light fw-bold'>Form</h2> 
         <div id="duplicater">  
           <div className='row'>
             <div className='col-6 input-div mt-2'>
-              <input type="text" className="form-control input-box" />
+              <input type="text" className="form-control input-box" /> 
             </div>
             <div className='col-6 input-div mt-2'>
               <input type="text" className="form-control input-box" />
@@ -25,7 +25,10 @@ export default function Form() {
           </div>
         </div>
       </div>
-      <button type="button" className="mt-2 button float-end"  onClick={duplicate}> + </button>
+      <div className='container mb-5'>
+        <button type="button" className='mt-3 btn submit-btn'> Submit </button>
+        <button type="button" className="mt-3 button float-end"  onClick={duplicate}> + </button>
+      </div>
     </div>  
   )
 } 
