49934aef14c3776d4844ec0730d365cf6cbfacd0
Gitesh Patil <patilgitesh29@gmail.com>
2022-05-10 19:36:59 +0530

    Frontend for Dynamic Form completed

--- medical-records-on-blockchain/src/Components/Form.js
+++ medical-records-on-blockchain/src/Components/Form.js
@@ -1,17 +1,31 @@
 import React from 'react'
 
-export default function Form() {
-  return (
-    <div className='container'>
-        <div className='row'>
-            <div className='col-6 input-div my-5'>
-                <input type="text" className="form-control input-box"/>
+export default function Form() { 
+
+  var original = document.getElementById('duplicater');
+
+  const duplicate = () => {
+    var clone = original.cloneNode(true);
+    clone.id = ""; 
+    original.parentNode.appendChild(clone);
+  } 
+ 
+  return ( 
+    <div>
+      <div className='container pt-4'>    
+        <h2 className='mb-3 text-center fw-bold'>Form</h2> 
+        <div id="duplicater">  
+          <div className='row'>
+            <div className='col-6 input-div mt-2'>
+              <input type="text" className="form-control input-box" />
             </div>
-            <div className='col-6 input-div my-5'>
-                <input type="text" className="form-control input-box"/>
+            <div className='col-6 input-div mt-2'>
+              <input type="text" className="form-control input-box" />
             </div>
-            <button type="button" style={{width:"3.2%"}} class="btn btn-primary"> + </button>
+          </div>
         </div>
-    </div>
+      </div>
+      <button type="button" className="mt-2 button float-end"  onClick={duplicate}> + </button>
+    </div>  
   )
-}
+} 
